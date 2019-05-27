53b3e1a5c60db14c36cc0e3459cdd37c29297754
Jakob Lachermeier <jakob.lachermeier98@gmail.com>
2019-05-27 19:02:10 +0200

    added user story for rock paper scissors discord bot

---
+++ rock_paper_scissors_bot/src/main.rs
@@ -0,0 +1,18 @@
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+/**
+The user types !kliklaklu in the chat.
+He receives a private message telling him he has to wait for another player to join.
+The bot sends out a message in the global chat prompting a user to join the initiator
+by typing !accept in the chat.
+
+The bot asks both players to send their choice
+it posts the result in both the private chats and the public one
+if its a draw both players have to type their choice again.
+
+*/
